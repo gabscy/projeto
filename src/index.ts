1fd9e3762c89e3248c2d88e25239794a397f51d7
gabscy <gabrielyin123@gmail.com>
2025-05-20 15:20:16 -0300

    port

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import cors from "cors";
 dotenv.config();
 
 export const app = express();
-const port = 3000;
+const port = ProcessingInstruction.env.PORT || 3000;
 const quadraController = new QuadraController();
 const slotController = new SlotController();
 const reservaController = new ReservaController();
